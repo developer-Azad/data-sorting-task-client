08e8325e5551fad7eb3d52859c3e93a642e53bb4
SyedHasibRahman <syedhasib01@gmail.com>
2022-01-03 21:41:47 +0600

    Old

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -5,40 +5,44 @@ import chair from '../../../images/chair.png';
 import bg from '../../../images/bg.png'
 import { Button, Typography, Container } from '@mui/material';
 
-const bannerBg={
-    background: `url(${bg})`
-    
+const bannerBg = {
+  background: `url(${bg})`
+
 }
 
 const verticalCenter = {
-    display: 'flex',
-    alignItems: 'center',
-    height: 400,
+  display: 'flex',
+  alignItems: 'center',
+  height: 400,
 }
 
 const Banner = () => {
-    return (
-        <Container style={bannerBg} sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item style={{...verticalCenter, textAlign: 'left'}} xs={12} md={5}>
+  return (
+    <Container style={ bannerBg } sx={ { flexGrow: 1 } }>
+      <Grid container spacing={ 2 }>
+        <Grid item style={ { ...verticalCenter, textAlign: 'left' } } xs={ 12 } md={ 5 }>
           <Box>
-          <Typography variant="h3" sx={{mb: 6}} >
-            Your New smile <br />
-            starts Here
-          </Typography>
-          <Typography variant="h7" sx={{ my: 3,  color: 'gray'}}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit tempora perferendis minus delectus fugiat iure voluptatibus quibusdam consectetur nesciunt corporis hic, aspernatur corrupti vitae nobis molestiae deleniti, dolor recusandae provident!
-          </Typography>
-          <br /><br />
-          <Button variant="contained" style={{backgroundColor: '#5CE7ED'}}>Get Appointment</Button>
+            <Typography variant="h3" sx={ { mb: 6 } } >
+              Your New smile <br />
+              starts Here
+            </Typography>
+            <Typography variant="h3" sx={ { mb: 6 } } >
+              Your Old smile <br />
+              starts Here
+            </Typography>
+            <Typography variant="h7" sx={ { my: 3, color: 'gray' } }>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit tempora perferendis minus delectus fugiat iure voluptatibus quibusdam consectetur nesciunt corporis hic, aspernatur corrupti vitae nobis molestiae deleniti, dolor recusandae provident!
+            </Typography>
+            <br /><br />
+            <Button variant="contained" style={ { backgroundColor: '#5CE7ED' } }>Get Appointment</Button>
           </Box>
-          </Grid>
-          <Grid item xs={12} md={6} style={verticalCenter}>
-            <img style={{width: '350px'}} src={chair} alt="" />
-          </Grid>
         </Grid>
-      </Container>
-    );
+        <Grid item xs={ 12 } md={ 6 } style={ verticalCenter }>
+          <img style={ { width: '350px' } } src={ chair } alt="" />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Banner;
